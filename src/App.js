6397e8af20c575a27f6dda95a2361d321792af5d
Tino Echaniz <tino@tinux.net>
2023-07-15 18:12:23 +0200

    Inicializacion de la Calculado9ra

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.svg';
 
 function App() {
+	return (
+		<div className="App">
+			<div className='freecodecamp-logo-contenedor'>	
+				<img src={freeCodeCampLogo}
+				className='freecodecamp-logo'
+				alt='Logode freCodeCamp' />
+			</div>
+			<div className='contenedor-calculadora'>
+			</div>
+		</div>
+	)
 }
 
 export default App;
